dab3539398c5ccb2dec0a2dd2a3d6b63ec1513b5
after23 <afiftrifarhan@gmail.com>
2023-05-29 22:43:33 +0700

    edit response async

--- handlers/commandHandlers.go
+++ handlers/commandHandlers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -22,27 +23,37 @@ func Absen(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
 	embed := &discordgo.MessageEmbed{
 		Title: "Absen",
-		Description: "Status : Processing",
+		Description: "Status: Processing",
 		Timestamp: time.Now().Format(time.RFC3339),
 	}
+	embeds := []*discordgo.MessageEmbed{embed}
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Embeds: []*discordgo.MessageEmbed{embed},
+			Embeds: embeds,
 		},
 	})
-	go absen(s, i.Interaction, "https://telat-api.onrender.com/talenta/absen/?api-key=hQcx29p8gWXyq6wdQykFAxcpb8bqnwsx")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go absen(&wg, s, i.Interaction, "https://telat-api.onrender.com/talenta/absen/?api-key=hQcx29p8gWXyq6wdQykFAxcpb8bqnwsx")
+	wg.Wait()
+	embeds[0].Description = "Status: Finished"
+	s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+		Embeds: &embeds,
+	})
 }
 
-func absen(s *discordgo.Session , i *discordgo.Interaction, url string ) {
+func absen(wg *sync.WaitGroup, s *discordgo.Session , i *discordgo.Interaction, url string ) {
 		// Make an HTTP request to retrieve the image.
 		resp, err := http.Get(url)
 		if err != nil {
 			// fmt.Println("Error retrieving image:", err)
+			wg.Done()
 			log.Panic("Error doing http request ", err)
 		}
 		defer resp.Body.Close()
 		if resp.StatusCode != http.StatusOK {
+			wg.Done()
 			log.Panic("somehing went oopsies ", err)
 		}
 
@@ -52,12 +63,13 @@ func absen(s *discordgo.Session , i *discordgo.Interaction, url string ) {
 		// Upload the file to Discord and get the attachment URL.
 		msg, err := s.ChannelFileSend("1112298002938347550" , "image.png", resp.Body)
 		if err != nil {
+			wg.Done()
 			log.Panic("error sending image :", err)
 		}
 
 		embed := &discordgo.MessageEmbed{
 				Title: "Absen Result",
-				Description: "Absen success",
+				Description: "Success",
 				Image: &discordgo.MessageEmbedImage{
 					URL: msg.Attachments[0].URL, // URL of the image
 				},
@@ -67,19 +79,20 @@ func absen(s *discordgo.Session , i *discordgo.Interaction, url string ) {
 			if err != nil {
 				fmt.Println("Error sending message:", err)
 		}
+		wg.Done()
 
-		embed = &discordgo.MessageEmbed{
-		Title: "Absen",
-		Description: "Status : Processing",
-		Timestamp: time.Now().Format(time.RFC3339),
-		}
+		// embed = &discordgo.MessageEmbed{
+		// Title: "Absen",
+		// Description: "Status : Processing",
+		// Timestamp: time.Now().Format(time.RFC3339),
+		// }
 
-		embeds := []*discordgo.MessageEmbed{embed}
+		// embeds := []*discordgo.MessageEmbed{embed}
 
-		updateResponse := &discordgo.WebhookEdit{
-			Embeds: &embeds,
-		}
+		// updateResponse := &discordgo.WebhookEdit{
+		// 	Embeds: &embeds,
+		// }
 		
-		s.InteractionResponseEdit(i, updateResponse)		
+		// s.InteractionResponseEdit(i, updateResponse)		
 		// return attachment.URL
 }
