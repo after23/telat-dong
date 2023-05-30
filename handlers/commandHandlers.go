de897524bd863a8d750576a87a4990157fd339d0
after23 <afiftrifarhan@gmail.com>
2023-05-30 16:33:58 +0700

    dockerization

--- handlers/commandHandlers.go
+++ handlers/commandHandlers.go
@@ -23,7 +23,7 @@ func Hello(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: "world",
+			Content: "Heya",
 		},
 	})
 }
